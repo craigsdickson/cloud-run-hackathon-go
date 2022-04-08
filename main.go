757666cb30f91670c2fab2bb158c261aaa8a70c8
Craig Dickson <craigsdickson@gmail.com>
2022-04-08 05:25:24 +0000

    implementated basic is player in front of me logic

--- main.go
+++ main.go
@@ -43,19 +43,78 @@ func handler(w http.ResponseWriter, req *http.Request) {
 
 func play(input ArenaUpdate) (response string) {
 	log.Printf("IN: %#v", input)
-	if someoneIsInFrontOfMe(input) {
+	board := generateBoard(input)
+	myState := extractMyState(input)
+	if someoneIsInFrontOfMe(myState, board) {
 		return "T"
 	} else {
-		return moveTowardsNextClosestPlayer(input)
+		return moveTowardsNextClosestPlayer(myState, board)
 	}
 }
 
-func moveTowardsNextClosestPlayer(input ArenaUpdate) (response string) {
+func extractMyState(input ArenaUpdate) PlayerState {
+	myId := input.Links.Self.Href
+	state := input.Arena.State
+	return state[myId]
+}
+
+// creates a 2D array of booleans representing locations of players on the board
+func generateBoard(input ArenaUpdate) [][]bool {
+	// generate the board data structure
+	width := input.Arena.Dimensions[0]
+	height := input.Arena.Dimensions[1]
+	var board = make([][]bool, width)
+	for i := range board {
+		board[i] = make([]bool, height)
+	}
+	// now populate board with locations of players
+	state := input.Arena.State
+	for _, v := range state {
+		vX := v.X
+		vY := v.Y
+		board[vX][vY] = true
+	}
+	return board
+}
+
+func moveTowardsNextClosestPlayer(myState PlayerState, board [][]bool) (response string) {
 	commands := []string{"F", "R", "L", "T"}
 	rand := rand2.Intn(4)
 	return commands[rand]
 }
 
-func someoneIsInFrontOfMe(input ArenaUpdate) (response bool) {
-	return false
+// determines if there is a player in firing line or not
+func someoneIsInFrontOfMe(myState PlayerState, board [][]bool) (response bool) {
+	myX := myState.X
+	myY := myState.Y
+	myDirection := myState.Direction
+	boardWidth := len(board)
+	boardHeight := len(board[0])
+	switch myDirection {
+	case "N":
+		if myY-1 >= 0 { // check we dont go outside north border
+			return board[myX][myY-1]
+		} else {
+			return false
+		}
+	case "E":
+		if myX+1 <= boardWidth { // check we dont go outside the east border
+			return board[myX+1][myY]
+		} else {
+			return false
+		}
+	case "S":
+		if myY+1 <= boardHeight { // check we dont go outside the south border
+			return board[myX][myY+1]
+		} else {
+			return false
+		}
+	default: // "W"
+		if myX-1 >= 0 { // check we dont go outside west border
+			return board[myX-1][myY]
+		} else {
+			return false
+		}
+	}
+
 }
